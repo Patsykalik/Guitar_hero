ef988761b82a29d1576ff4c79abb0e8d731e033c
Patsykalik <apacykalik@gmail.com>
2026-05-07 02:13:34 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     MainWindow w;
-    w.setWindowTitle("🎸 Guitar Hero Qt Project");
+    w.setWindowTitle("🎸 Guitar Hero");
     w.show();
 
     return a.exec();
