963a50aa1395053637d7343872e8c46c551985c7
Patsykalik <apacykalik@gmail.com>
2026-05-07 02:13:49 +0300

    Update gameplay.cpp

--- gameplay.cpp
+++ gameplay.cpp
@@ -10,9 +10,10 @@
 #include <QAudioOutput>
 #include <QLabel>
 #include <QPushButton>
-#include <QVBoxLayout>
 #include <QRandomGenerator>
 #include <QUrl>
+#include <QDir>
+#include <QKeyEvent>
 
 GuitarPlayer::GuitarPlayer() {
     setTransformOriginPoint(25, 25);
@@ -27,8 +28,6 @@ void GuitarPlayer::paint(QPainter *painter, const QStyleOptionGraphicsItem*, QWi
     painter->setBrush(grad);
     painter->setPen(Qt::NoPen);
     painter->drawEllipse(5, 10, 40, 40);
-    painter->setBrush(Qt::darkGray);
-    painter->drawRect(20, 0, 10, 15);
 }
 
 Amplifier::Amplifier() {
@@ -43,10 +42,7 @@ void Amplifier::paint(QPainter *painter, const QStyleOptionGraphicsItem*, QWidge
     grad.setColorAt(0, Qt::darkGray);
     grad.setColorAt(1, Qt::black);
     painter->setBrush(grad);
-    painter->setPen(QPen(Qt::white, 2));
     painter->drawRoundedRect(0, 0, 100, 100, 10, 10);
-    painter->setBrush(Qt::black);
-    painter->drawEllipse(20, 20, 60, 60);
     painter->setBrush(ledColor);
     painter->drawEllipse(75, 10, 15, 15);
 }
@@ -56,18 +52,6 @@ void Amplifier::resetColor() { ledColor = Qt::red; update(); }
 MusicNote::MusicNote(qreal x, qreal y) {
     setPos(x, y);
     setZValue(3);
-    QPropertyAnimation *move = new QPropertyAnimation(this, "y");
-    move->setDuration(800);
-    move->setStartValue(y);
-    move->setEndValue(y - 80);
-    move->start(QAbstractAnimation::DeleteWhenStopped);
-    QGraphicsOpacityEffect *effect = new QGraphicsOpacityEffect;
-    setGraphicsEffect(effect);
-    QPropertyAnimation *fade = new QPropertyAnimation(effect, "opacity");
-    fade->setDuration(800);
-    fade->setStartValue(1.0); fade->setEndValue(0.0);
-    fade->start(QAbstractAnimation::DeleteWhenStopped);
-    QTimer::singleShot(800, this, &QGraphicsObject::deleteLater);
 }
 QRectF MusicNote::boundingRect() const { return QRectF(0, 0, 20, 30); }
 void MusicNote::paint(QPainter *painter, const QStyleOptionGraphicsItem*, QWidget*) {
@@ -77,19 +61,14 @@ void MusicNote::paint(QPainter *painter, const QStyleOptionGraphicsItem*, QWidge
 }
 
 GameNoteSprite::GameNoteSprite(int stringIndex, const QColor& color) : strIdx(stringIndex), noteColor(color) {
-    QPixmap atlas("atlas.png");
-    if (!atlas.isNull()) sprite = atlas.copy(strIdx * 50, 0, 50, 50);
     setZValue(10);
 }
 QRectF GameNoteSprite::boundingRect() const { return QRectF(-25, -25, 50, 50); }
 void GameNoteSprite::paint(QPainter *painter, const QStyleOptionGraphicsItem*, QWidget*) {
-    if (!sprite.isNull()) painter->drawPixmap(-25, -25, sprite);
-    else {
-        painter->setRenderHint(QPainter::Antialiasing);
-        painter->setBrush(noteColor);
-        painter->setPen(QPen(Qt::white, 2));
-        painter->drawEllipse(-20, -20, 40, 40);
-    }
+    painter->setRenderHint(QPainter::Antialiasing);
+    painter->setBrush(noteColor);
+    painter->setPen(QPen(Qt::white, 2));
+    painter->drawEllipse(-20, -20, 40, 40);
 }
 
 GameWidget::GameWidget(QWidget *parent) : QWidget(parent) {
@@ -97,33 +76,54 @@ GameWidget::GameWidget(QWidget *parent) : QWidget(parent) {
     scene = new QGraphicsScene(0, 0, VIEW_WIDTH, VIEW_HEIGHT, this);
     view = new QGraphicsView(scene, this);
     view->setRenderHint(QPainter::Antialiasing);
-    view->setStyleSheet("background: #111;");
+    view->setStyleSheet("background: transparent; border: none;");
+    
     audioOutput = new QAudioOutput(this);
     mediaPlayer = new QMediaPlayer(this);
     mediaPlayer->setAudioOutput(audioOutput);
+    
     gameTimer = new QTimer(this);
     connect(gameTimer, &QTimer::timeout, this, &GameWidget::updatePhysics);
     spawnTimer = new QTimer(this);
     connect(spawnTimer, &QTimer::timeout, this, &GameWidget::spawnNote);
+
     countdownLabel = new QLabel(this);
     countdownLabel->setGeometry(VIEW_WIDTH/2-50, VIEW_HEIGHT/2-50, 100, 100);
     countdownLabel->setAlignment(Qt::AlignCenter);
     countdownLabel->setStyleSheet("color: white; font-size: 72px; font-weight: bold;");
-    countdownLabel->hide();
-    QPushButton *stopBtn = new QPushButton("Завершить", this);
-    stopBtn->setGeometry(10, 10, 100, 30);
+    
+    scoreLabel = new QLabel("Очки: 0", this);
+    scoreLabel->setGeometry(VIEW_WIDTH - 160, 20, 140, 40);
+    scoreLabel->setStyleSheet("color: #00FF00; font-size: 24px; font-weight: bold; background: rgba(0,0,0,150); padding: 5px;");
+
+    QPushButton *stopBtn = new QPushButton("Меню", this);
+    stopBtn->setGeometry(10, 10, 80, 30);
     connect(stopBtn, &QPushButton::clicked, this, &GameWidget::stopGame);
 }
 
 void GameWidget::startGame() {
-    scene->clear(); notes.clear(); drawFretboard();
-    mediaPlayer->setSource(QUrl::fromLocalFile(globalConfig.selectedSong));
-    countdownValue = 3; countdownLabel->setText(QString::number(countdownValue));
+    scene->clear(); 
+    notes.clear(); 
+    score = 0;
+    scoreLabel->setText("Очки: 0");
+
+    QPixmap bgPix("3ae8bceb83287ebf54c46b101eafd361.jpg");
+    if (!bgPix.isNull()) {
+        scene->setBackgroundBrush(bgPix.scaled(VIEW_WIDTH, VIEW_HEIGHT, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    }
+
+    drawFretboard(); 
+    this->setFocus(); 
+
+    QString songPath = QDir::current().absoluteFilePath(globalConfig.selectedSong);
+    mediaPlayer->setSource(QUrl::fromLocalFile(songPath));
+    audioOutput->setVolume(1.0);
+
+    countdownValue = 3;
     countdownLabel->show();
     QTimer *ctTimer = new QTimer(this);
     connect(ctTimer, &QTimer::timeout, [this, ctTimer]() {
-        countdownValue--;
-        if (countdownValue > 0) countdownLabel->setText(QString::number(countdownValue));
+        if (--countdownValue > 0) countdownLabel->setText(QString::number(countdownValue));
         else if (countdownValue == 0) countdownLabel->setText("GO!");
         else {
             countdownLabel->hide(); ctTimer->stop(); ctTimer->deleteLater();
@@ -133,46 +133,47 @@ void GameWidget::startGame() {
     ctTimer->start(1000);
 }
 
-void GameWidget::drawFretboard() {
-    QPen pen(Qt::lightGray, 4);
-    int n = globalConfig.numStrings;
-    for (int i = 0; i < n; ++i) {
-        qreal topX, bottomX;
-        if (globalConfig.inclinedFretboard) {
-            topX = VIEW_WIDTH/2.0 + (i-(n-1)/2.0)*30.0;
-            bottomX = VIEW_WIDTH/2.0 + (i-(n-1)/2.0)*100.0;
-        } else {
-            topX = VIEW_WIDTH/2.0 + (i-(n-1)/2.0)*80.0;
-            bottomX = topX;
+void GameWidget::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_1) checkHit(0);
+    else if (event->key() == Qt::Key_2) checkHit(1);
+    else if (event->key() == Qt::Key_3) checkHit(2);
+    else if (event->key() == Qt::Key_4) checkHit(3);
+}
+
+void GameWidget::checkHit(int stringIdx) {
+    for (int i = 0; i < notes.size(); ++i) {
+        if (notes[i]->strIdx == stringIdx && notes[i]->progress > 0.85 && notes[i]->progress < 1.1) {
+            score += 10;
+            scoreLabel->setText(QString("Очки: %1").arg(score));
+            scene->removeItem(notes[i]);
+            delete notes.takeAt(i);
+            return;
         }
-        scene->addLine(topX, 50, bottomX, VIEW_HEIGHT-50, pen);
     }
-    scene->addLine(0, VIEW_HEIGHT-60, VIEW_WIDTH, VIEW_HEIGHT-60, QPen(Qt::red, 6));
+}
+
+void GameWidget::drawFretboard() {
+    QPen pen(QColor(255, 255, 255, 80), 4);
+    for (int i = 0; i < globalConfig.numStrings; ++i) {
+        qreal x = VIEW_WIDTH/2.0 + (i-(globalConfig.numStrings-1)/2.0)*80.0;
+        scene->addLine(x, 0, x, VIEW_HEIGHT, pen);
+    }
+    scene->addLine(0, VIEW_HEIGHT-60, VIEW_WIDTH, VIEW_HEIGHT-60, QPen(Qt::red, 5));
 }
 
 void GameWidget::spawnNote() {
-    int stringIdx = QRandomGenerator::global()->bounded(globalConfig.numStrings);
-    QList<QColor> colors = {Qt::green, Qt::red, Qt::yellow, Qt::blue, Qt::magenta};
-    GameNoteSprite *note = new GameNoteSprite(stringIdx, colors[stringIdx % colors.size()]);
-    scene->addItem(note); notes.append(note);
+    int s = QRandomGenerator::global()->bounded(globalConfig.numStrings);
+    QList<QColor> c = {Qt::green, Qt::red, Qt::yellow, Qt::blue};
+    GameNoteSprite *n = new GameNoteSprite(s, c[s % c.size()]);
+    scene->addItem(n); notes.append(n);
 }
 
 void GameWidget::updatePhysics() {
-    int n = globalConfig.numStrings;
     for (int i = notes.size()-1; i >= 0; --i) {
-        GameNoteSprite *note = notes[i];
-        note->progress += 0.01;
-        qreal topX, bottomX;
-        if (globalConfig.inclinedFretboard) {
-            topX = VIEW_WIDTH/2.0 + (note->strIdx-(n-1)/2.0)*30.0;
-            bottomX = VIEW_WIDTH/2.0 + (note->strIdx-(n-1)/2.0)*100.0;
-            note->setScale(0.5 + note->progress*0.7);
-        } else {
-            topX = VIEW_WIDTH/2.0 + (note->strIdx-(n-1)/2.0)*80.0;
-            bottomX = topX;
-        }
-        note->setPos(topX + (bottomX-topX)*note->progress, 50 + (VIEW_HEIGHT-100)*note->progress);
-        if (note->progress >= 1.1) { scene->removeItem(note); notes.removeAt(i); delete note; }
+        notes[i]->progress += 0.008;
+        qreal x = VIEW_WIDTH/2.0 + (notes[i]->strIdx-(globalConfig.numStrings-1)/2.0)*80.0;
+        notes[i]->setPos(x, 50 + (VIEW_HEIGHT-100)*notes[i]->progress);
+        if (notes[i]->progress >= 1.2) { scene->removeItem(notes[i]); delete notes.takeAt(i); }
     }
 }
 
