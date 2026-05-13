bb00b7507d9ad02f470a5b0b5f1f563f4d6d1572
Patsykalik <apacykalik@gmail.com>
2026-05-13 12:07:20 +0300

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -1,6 +1,5 @@
 #include "interface.h"
-#include <QGraphicsScene>
-#include <QGraphicsView>
+
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QPushButton>
@@ -9,171 +8,483 @@
 #include <QSpinBox>
 #include <QRadioButton>
 #include <QStackedWidget>
-#include <QtMath>
 #include <QApplication>
-#include <QTimer>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QtMath>
+#include <QTableWidget>
+#include <QHeaderView>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QFile>
+#include <QGraphicsDropShadowEffect>
+#include <QPropertyAnimation>
+#include <QCoreApplication>
 
-RockScreenSaver::RockScreenSaver(QWidget *parent) : QWidget(parent) {
-    setFixedSize(VIEW_WIDTH, VIEW_HEIGHT);
+
+RockScreenSaver::RockScreenSaver(QWidget *parent)
+    : QWidget(parent)
+{
     scene = new QGraphicsScene(0, 0, VIEW_WIDTH, VIEW_HEIGHT, this);
+
     scene->setBackgroundBrush(Qt::black);
 
+    view = new QGraphicsView(scene, this);
+
+    view->setStyleSheet("background: transparent; border: none;");
+
     amplifier = new Amplifier();
-    amplifier->setPos(350, 250);
+    amplifier->setPos(375, 275);
+
     scene->addItem(amplifier);
 
     player = new GuitarPlayer();
+
     scene->addItem(player);
 
-    view = new QGraphicsView(scene, this);
-    view->setRenderHint(QPainter::Antialiasing);
-    view->setStyleSheet("background: transparent; border: none;");
-
     selector = new QComboBox();
-    selector->addItems({"🎸 Круг", "🎸 Синус", "🎸 Восьмёрка"});
 
-    QPushButton *backBtn = new QPushButton("Назад в меню");
-    connect(backBtn, &QPushButton::clicked, this, &RockScreenSaver::returnToMenu);
+    selector->addItems({"Круг", "Синус"});
 
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(view);
-    layout->addWidget(selector);
-    layout->addWidget(backBtn);
+    QPushButton *btn = new QPushButton("НАЗАД");
+
+    connect(btn,
+            &QPushButton::clicked,
+            this,
+            &RockScreenSaver::returnToMenu);
+
+    QVBoxLayout *l = new QVBoxLayout(this);
+
+    l->addWidget(view);
+    l->addWidget(selector);
+    l->addWidget(btn);
 
     timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this, &RockScreenSaver::animate);
+
+    connect(timer,
+            &QTimer::timeout,
+            this,
+            &RockScreenSaver::animate);
 }
 
-void RockScreenSaver::startAnimation() { timer->start(16); }
-void RockScreenSaver::stopAnimation() { timer->stop(); }
+void RockScreenSaver::startAnimation()
+{
+    timer->start(16);
+}
 
-void RockScreenSaver::animate() {
+void RockScreenSaver::stopAnimation()
+{
+    timer->stop();
+}
+
+void RockScreenSaver::animate()
+{
     angle += 0.05;
-    qreal x = 400, y = 300;
-    int mode = selector->currentIndex();
 
-    if (mode == 0) { 
-        x = 400 + 150 * qCos(angle);
-        y = 300 + 150 * qSin(angle);
-    } else if (mode == 1) { 
-        x = 100 + fmod(angle * 50, 600);
-        y = 300 + 100 * qSin(angle);
-    } else { 
+    qreal x;
+    qreal y;
+
+    if (selector->currentIndex() == 0) {
+
         x = 400 + 200 * qCos(angle);
-        y = 300 + 100 * qSin(2 * angle);
+        y = 300 + 200 * qSin(angle);
+
+    } else {
+
+        x = 400 + 200 * qCos(angle);
+        y = 300 + 100 * qSin(angle * 2);
     }
 
     player->setPos(x - 25, y - 25);
-    player->setRotation(angle * 50);
 
     if (player->collidesWithItem(amplifier)) {
+
         if (!wasColliding) {
+
             amplifier->flash();
-            scene->addItem(new MusicNote(amplifier->x() + 50, amplifier->y()));
+
             wasColliding = true;
         }
+
     } else {
+
         wasColliding = false;
     }
 }
 
-SettingsWidget::SettingsWidget(QWidget *parent) : QWidget(parent) {
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    QLabel *title = new QLabel("Настройки гитары");
-    title->setAlignment(Qt::AlignCenter);
-    title->setStyleSheet("font-size: 24px; font-weight: bold;");
 
-    QHBoxLayout *hBox = new QHBoxLayout();
-    hBox->addWidget(new QLabel("Количество струн (1-4):"));
+SettingsWidget::SettingsWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout *l = new QVBoxLayout(this);
+
     stringsSpin = new QSpinBox();
-    stringsSpin->setRange(1, 4);
+
+    stringsSpin->setRange(2, 4);
+
+    verticalRadio = new QRadioButton("Вертикально");
+
+    inclinedRadio = new QRadioButton("Наклон");
+
+    QFile loadFile(QCoreApplication::applicationDirPath() + "/settings.json");
+
+    if (loadFile.open(QIODevice::ReadOnly)) {
+
+        QByteArray data = loadFile.readAll();
+
+        QJsonDocument doc = QJsonDocument::fromJson(data);
+
+        QJsonObject obj = doc.object();
+
+        globalConfig.numStrings =
+            obj["numStrings"].toInt(4);
+
+        globalConfig.inclinedFretboard =
+            obj["inclinedFretboard"].toBool(true);
+
+        loadFile.close();
+    }
+
     stringsSpin->setValue(globalConfig.numStrings);
-    hBox->addWidget(stringsSpin);
 
-    verticalRadio = new QRadioButton("Прямой гриф");
-    inclinedRadio = new QRadioButton("Наклонный гриф");
-    if (globalConfig.inclinedFretboard) inclinedRadio->setChecked(true);
-    else verticalRadio->setChecked(true);
+    if (globalConfig.inclinedFretboard)
+        inclinedRadio->setChecked(true);
+    else
+        verticalRadio->setChecked(true);
 
-    QPushButton *saveBtn = new QPushButton("Сохранить и выйти");
-    
-    layout->addWidget(title);
-    layout->addLayout(hBox);
-    layout->addWidget(verticalRadio);
-    layout->addWidget(inclinedRadio);
-    layout->addWidget(saveBtn);
+    QPushButton *b = new QPushButton("ПРИМЕНИТЬ");
 
-    connect(saveBtn, &QPushButton::clicked, [this]() {
-        globalConfig.numStrings = stringsSpin->value(); 
-        globalConfig.inclinedFretboard = inclinedRadio->isChecked();
+    l->addWidget(new QLabel("КОЛИЧЕСТВО СТРУН:"));
+    l->addWidget(stringsSpin);
+    l->addWidget(verticalRadio);
+    l->addWidget(inclinedRadio);
+    l->addWidget(b);
+
+    connect(b,
+            &QPushButton::clicked,
+            [this]() {
+
+        globalConfig.numStrings =
+            stringsSpin->value();
+
+        globalConfig.inclinedFretboard =
+            inclinedRadio->isChecked();
+
+        QJsonObject obj;
+
+        obj["numStrings"] =
+            globalConfig.numStrings;
+
+        obj["inclinedFretboard"] =
+            globalConfig.inclinedFretboard;
+
+        QJsonDocument doc(obj);
+
+        QFile saveFile(
+            QCoreApplication::applicationDirPath()
+            + "/settings.json"
+        );
+
+        if (saveFile.open(QIODevice::WriteOnly)) {
+
+            saveFile.write(doc.toJson());
+
+            saveFile.close();
+        }
+
         emit returnToMenu();
     });
 }
 
-MainMenuWidget::MainMenuWidget(QWidget *parent) : QWidget(parent) {
+
+LeaderboardWidget::LeaderboardWidget(QWidget *parent)
+    : QWidget(parent)
+{
     QVBoxLayout *layout = new QVBoxLayout(this);
-    QLabel *logo = new QLabel("GUITAR HERO");
-    logo->setAlignment(Qt::AlignCenter);
-    logo->setStyleSheet("font-size: 40px; color: red; font-weight: bold;");
 
-    songSelector = new QComboBox();
-    songSelector->addItems({"song1.mp3", "rock_track.mp3", "solo.mp3"});
+    QLabel *title =
+        new QLabel("ТАБЛИЦА ЛИДЕРОВ");
 
-    QPushButton *playBtn = new QPushButton("ИГРАТЬ");
-    QPushButton *settingsBtn = new QPushButton("НАСТРОЙКИ");
-    QPushButton *screensaverBtn = new QPushButton("ЗАСТАВКА");
+    title->setStyleSheet(
+        "font-size: 42px;"
+        "font-weight: bold;"
+        "color: #ffaa44;"
+        "border-bottom: 3px solid #ffaa44;"
+        "padding-bottom: 15px;"
+    );
 
-    layout->addWidget(logo);
-    layout->addWidget(new QLabel("Выберите трек:"));
-    layout->addWidget(songSelector);
-    layout->addWidget(playBtn);
-    layout->addWidget(settingsBtn);
-    layout->addWidget(screensaverBtn);
+    title->setAlignment(Qt::AlignCenter);
 
-    connect(playBtn, &QPushButton::clicked, [this]() {
-        globalConfig.selectedSong = songSelector->currentText();
-        emit startGame();
-    });
-    connect(settingsBtn, &QPushButton::clicked, this, &MainMenuWidget::openSettings);
-    connect(screensaverBtn, &QPushButton::clicked, this, &MainMenuWidget::openScreensaver);
+    table = new QTableWidget();
+
+    table->setColumnCount(3);
+
+   table->setColumnCount(4);
+
+    table->setHorizontalHeaderLabels({
+    "ИМЯ",
+    "ПЕСНЯ",
+    "ОЧКИ",
+    "ДАТА"
+ });
+
+    table->horizontalHeader()->
+            setSectionResizeMode(
+                QHeaderView::Stretch
+            );
+
+    QPushButton *back =
+        new QPushButton("НАЗАД");
+
+    connect(back,
+            &QPushButton::clicked,
+            this,
+            &LeaderboardWidget::returnToMenu);
+
+    layout->addWidget(title);
+    layout->addWidget(table);
+    layout->addWidget(back);
 }
 
-MainWindow::MainWindow() {
-    setFixedSize(VIEW_WIDTH, VIEW_HEIGHT);
-    stackedWidget = new QStackedWidget(this);
+void LeaderboardWidget::loadScores()
+{
+    table->setRowCount(0);
 
-    mainMenu = new MainMenuWidget();
-    settings = new SettingsWidget();
-    game = new GameWidget();
-    screenSaver = new RockScreenSaver();
+    QSqlQuery query(
+        "SELECT player_name, song_name, score, played_at "
+        "FROM leaderboard "
+        "ORDER BY score DESC"
+    );
 
-    stackedWidget->addWidget(mainMenu);    
-    stackedWidget->addWidget(settings);    
-    stackedWidget->addWidget(game);        
-    stackedWidget->addWidget(screenSaver); 
+    int row = 0;
 
-    connect(mainMenu, &MainMenuWidget::openSettings, [this](){ stackedWidget->setCurrentIndex(1); });
-    
-    connect(mainMenu, &MainMenuWidget::startGame, [this](){ 
-        stackedWidget->setCurrentIndex(2); 
-        game->startGame(); 
-        game->setFocus(); 
+    while (query.next()) {
+
+        table->insertRow(row);
+
+        table->setItem(
+            row,
+            0,
+            new QTableWidgetItem(
+                query.value(0).toString()
+            )
+        );
+
+        table->setItem(
+            row,
+            1,
+            new QTableWidgetItem(
+                query.value(1).toString()
+            )
+        );
+
+        table->setItem(
+            row,
+            2,
+            new QTableWidgetItem(
+                query.value(2).toString()
+            )
+        );
+
+        table->setItem(
+            row,
+            3,
+            new QTableWidgetItem(
+                query.value(3).toString()
+            )
+        );
+
+        row++;
+    }
+}
+
+MainMenuWidget::MainMenuWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout *l = new QVBoxLayout(this);
+
+    l->setSpacing(25);
+
+    QLabel *logo =
+        new QLabel("🎸GUITAR HERO");
+
+    logo->setStyleSheet(
+        "font-size: 52px;"
+        "font-weight: 900;"
+        "color: #ffaa44;"
+        "letter-spacing: 3px;"
+    );
+
+    logo->setAlignment(Qt::AlignCenter);
+
+    songSelector = new QComboBox();
+
+    songSelector->addItems({
+        "Zhit_v_kayf.mp3",
+        "Freedom.mp3",
+        "Don_Omar.mp3"
     });
 
-    connect(mainMenu, &MainMenuWidget::openScreensaver, [this](){ 
-        stackedWidget->setCurrentIndex(3); 
-        screenSaver->startAnimation(); 
+    QPushButton *b1 =
+        new QPushButton("▶ ИГРАТЬ");
+
+    QPushButton *b2 =
+        new QPushButton("⚙ НАСТРОЙКИ");
+
+    QPushButton *b3 =
+        new QPushButton("🎬 ЗАСТАВКА");
+
+    QPushButton *b4 =
+        new QPushButton("🏆 ТАБЛИЦА ЛИДЕРОВ");
+
+    QPushButton *b5 =
+        new QPushButton("✖ ВЫХОД");
+
+    l->addWidget(logo);
+    l->addWidget(songSelector);
+    l->addWidget(b1);
+    l->addWidget(b2);
+    l->addWidget(b3);
+    l->addWidget(b4);
+    l->addWidget(b5);
+
+    connect(b1,
+            &QPushButton::clicked,
+            [this]() {
+
+        globalConfig.selectedSong =
+            songSelector->currentText();
+
+        emit startGame();
     });
 
-    connect(settings, &SettingsWidget::returnToMenu, [this](){ stackedWidget->setCurrentIndex(0); });
-    connect(game, &GameWidget::returnToMenu, [this](){ stackedWidget->setCurrentIndex(0); });
-    
-    connect(screenSaver, &RockScreenSaver::returnToMenu, [this](){ 
-        screenSaver->stopAnimation(); 
-        stackedWidget->setCurrentIndex(0); 
+    connect(b2,
+            &QPushButton::clicked,
+            this,
+            &MainMenuWidget::openSettings);
+
+    connect(b3,
+            &QPushButton::clicked,
+            this,
+            &MainMenuWidget::openScreensaver);
+
+    connect(b4,
+            &QPushButton::clicked,
+            this,
+            &MainMenuWidget::openLeaderboard);
+
+    connect(b5,
+            &QPushButton::clicked,
+            qApp,
+            &QApplication::quit);
+}
+
+
+MainWindow::MainWindow()
+{
+    stackedWidget =
+        new QStackedWidget(this);
+
+    mainMenu =
+        new MainMenuWidget();
+
+    settings =
+        new SettingsWidget();
+
+    game =
+        new GameWidget();
+
+    screenSaver =
+        new RockScreenSaver();
+
+    leaderboard =
+        new LeaderboardWidget();
+
+    stackedWidget->addWidget(mainMenu);
+    stackedWidget->addWidget(settings);
+    stackedWidget->addWidget(game);
+    stackedWidget->addWidget(screenSaver);
+    stackedWidget->addWidget(leaderboard);
+
+    connect(mainMenu,
+            &MainMenuWidget::openSettings,
+            [this]() {
+
+        stackedWidget->setCurrentIndex(1);
     });
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->addWidget(stackedWidget);
-    setLayout(mainLayout);
+    connect(mainMenu,
+            &MainMenuWidget::startGame,
+            [this]() {
+
+        stackedWidget->setCurrentIndex(2);
+
+        game->startGame();
+
+        game->setFocus();
+    });
+
+    connect(mainMenu,
+            &MainMenuWidget::openScreensaver,
+            [this]() {
+
+        stackedWidget->setCurrentIndex(3);
+
+        screenSaver->startAnimation();
+    });
+
+    connect(mainMenu,
+            &MainMenuWidget::openLeaderboard,
+            [this]() {
+
+        leaderboard->loadScores();
+
+        stackedWidget->setCurrentWidget(
+            leaderboard
+        );
+    });
+
+    connect(settings,
+            &SettingsWidget::returnToMenu,
+            [this]() {
+
+        stackedWidget->setCurrentIndex(0);
+    });
+
+    connect(game,
+            &GameWidget::returnToMenu,
+            [this]() {
+
+        stackedWidget->setCurrentIndex(0);
+    });
+
+    connect(screenSaver,
+            &RockScreenSaver::returnToMenu,
+            [this]() {
+
+        screenSaver->stopAnimation();
+
+        stackedWidget->setCurrentIndex(0);
+    });
+
+    connect(leaderboard,
+            &LeaderboardWidget::returnToMenu,
+            [this]() {
+
+        stackedWidget->setCurrentIndex(0);
+    });
+
+    QVBoxLayout *l =
+        new QVBoxLayout(this);
+
+    l->setContentsMargins(0,0,0,0);
+
+    l->addWidget(stackedWidget);
+
+    resize(VIEW_WIDTH, VIEW_HEIGHT);
+
+    setMinimumSize(900, 600);
 }
